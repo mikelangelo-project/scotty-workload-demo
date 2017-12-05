37e070363c11181a6f1bbd1c9d398377d4cad754
Peter Chronz <peter.chronz@gmail.com>
2017-12-05 16:56:21 +0100

    Autopep8 on workload_gen

--- workload_gen.py
+++ workload_gen.py
@@ -4,14 +4,16 @@ from scotty import utils
 
 logger = logging.getLogger(__name__)
 
+
 def run(context):
     workload = context.v1.workload
     exp_helper = utils.ExperimentHelper(context)
-#    my_resource = exp_helper.get_resource(workload.resources['my_resource']
+    #    my_resource = exp_helper.get_resource(workload.resources['my_resource']
     logger.info('{}'.format(workload.params['greeting']))
     logger.info('I\'m workload generator {}'.format(workload.name))
-#    logger.info('The resource endpoint is {}'.format(my_resource))
+    #    logger.info('The resource endpoint is {}'.format(my_resource))
     return result_description
+
 
 def clean(context):
     pass
