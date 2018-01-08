0e2720e1d60db7e9b3e07405b670b84a8e0a3d2e
Maik Srba <msrba@gwdg.de>
2018-01-08 10:21:16 +0100

    implement new collect interface

--- workload_gen.py
+++ workload_gen.py
@@ -7,12 +7,13 @@ logger = logging.getLogger(__name__)
 def run(context):
     workload = context.v1.workload
     exp_helper = utils.ExperimentHelper(context)
-#    my_resource = exp_helper.get_resource(workload.resources['my_resource']
     logger.info('{}'.format(workload.params['greeting']))
     logger.info('I\'m workload generator {}'.format(workload.name))
-#    logger.info('The resource endpoint is {}'.format(my_resource))
-    result = {"result": "result"}
-    return result
+
+def collect(context):
+    workload_utils = utils.WorkloadUtils(context)
+    with workload_utils.open_file('my_result.txt', 'w+') as f:
+        f.write('my result content')
 
 def clean(context):
     pass
